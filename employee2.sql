5886c456c2f5907f3a6551f6f7524ee60b308ab2
armin <armin.oldboy@gmail.com>
2017-07-29 17:28:08 +0430

    change 1

--- employee2.sql
+++
@@ -1,7 +0,0 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE employees(id integer primary key,name text);
-INSERT INTO employees VALUES(1,'armin');
-INSERT INTO employees VALUES(2,'hamid');
-COMMIT;
-***a**
